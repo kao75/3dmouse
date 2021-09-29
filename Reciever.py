c0e4360f38f43f600ded1f52a8131aac5591384a
kao75 <kao75@pitt.edu>
2021-09-29 15:24:09 -0400

    modified Reciever.py

--- Reciever.py
+++ Reciever.py
@@ -5,14 +5,61 @@ import threading
 
 
 class Reciever:
+    """
+    A class used to represent the USB Reciever module
+    ...
+    Attributes
+    ----------
+    reciever : serial.Serial
+        a pyserial serial communication object responsible for fetching and sending data to reciever
+    timing : float
+        the timing delay between fetch data requests when streaming is enabled (default value is 0.01)
+    streaming : bool
+        bool value that enables/disables automatic data fetching, if disabled fetch_data can be called manually
+    mode : int
+        the mode of operation the mouse is in (valid values: 1, 2, or 3)
+    x : int
+        relative x delta displacement from last data fetch (valid values: 0-1023)
+    y : int
+        relative y delta displacement from last data fetch (valid values: 0-1023)
+    z : int
+        relative z delta displacement from last data fetch (valid values: 0-1023)
+
+    Methods
+    -------
+    start_stream()
+        start streaming data from reciever through a new threaded timer
+    end_stream()
+        stop streaming data from reciever
+    write_data()
+        send single char data to reciever
+    read_data()
+        read current mode, x, y, and z stored in object
+    fetch_data()
+        send 'w' request to reciever and in turn fetch new mode, x, y, z data to update the object"
+    close()
+        stop streaming and close serial connection with reciever
+    """
+
     def __init__(self, timing=.01):
+        """
+        Parameters
+        ----------
+        timing : float, optional
+            The timing delay between fetch data requests when streaming is enabled (default value is 0.01)
+
+        Raises
+        ------
+        RuntimeError
+            If no available COM ports detected or reciever not found in the available COM ports.
+        """
         ports = serial.tools.list_ports.comports(include_links=False)
         if len(ports) == 0:
             raise RuntimeError('No available COM ports detected.')
         self.reciever = None
         for port in ports:
             if port.description.startswith('Arduino NANO Every'):
-                self.reciever = serial.Serial(port=port.device, baudrate=115200, timeout=.2)
+                self.reciever = serial.Serial(port=port.device, baudrate=115200, timeout=.1)
                 break
         if self.reciever is None:
             raise RuntimeError('Reciever not found in available COM ports')
@@ -24,20 +71,31 @@ class Reciever:
         self.z = 0
 
     def start_stream(self):
+        """function to start streaming data from reciever through a new threaded timer"""
         self.streaming = True
         t = threading.Timer(0, self.fetch_data)
         t.start()
 
     def end_stream(self):
+        """function to stop streaming data from reciever"""
         self.streaming = False
 
     def write_data(self, char):
+        """function to send single char data to reciever
+
+        Parameters
+        ----------
+        char : char
+            The character to write to the reciever
+        """
         self.reciever.write(char.encode('utf8'))
 
     def read_data(self):
+        """function to read current mode, x, y, and z stored in object"""
         return self.mode, self.x, self.y, self.z
 
     def fetch_data(self):
+        """function to send 'w' request to reciever and in turn fetch new mode, x, y, z data to update the object"""
         self.write_data('w')
         data = self.reciever.read_until(bytes('\n', 'utf-8')).decode("utf-8")
         parsed = data.split('\t')
@@ -63,6 +121,7 @@ class Reciever:
         return self.mode, self.x, self.y, self.z
 
     def close(self):
+        """function to stop streaming and close serial connection with reciever"""
         self.streaming = False
         time.sleep(1)
         self.reciever.close()
@@ -76,7 +135,7 @@ def main():
     while True:
         mode, x, y, z = reciever.read_data()
         print(mode, x, y, z)
-        time.sleep(.25)
+        time.sleep(.1)
 
     reciever.end_stream()
     print('Ending Stream')
