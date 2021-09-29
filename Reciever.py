bebc5b32debe2a3678eca666aee5aebec869ac89
kao75 <kao75@pitt.edu>
2021-09-29 13:23:52 -0400

    modified Reciever.py and Trackball_Sim.ino

--- Reciever.py
+++ Reciever.py
@@ -1,11 +1,21 @@
 import time
 import serial
+import serial.tools.list_ports
 import threading
 
 
 class Reciever:
-    def __init__(self, timing=.5):
-        self.arduino = serial.Serial(port='COM5', baudrate=115200, timeout=2)
+    def __init__(self, timing=.01):
+        ports = serial.tools.list_ports.comports(include_links=False)
+        if len(ports) == 0:
+            raise RuntimeError('No available COM ports detected.')
+        self.reciever = None
+        for port in ports:
+            if port.description.startswith('Arduino NANO Every'):
+                self.reciever = serial.Serial(port=port.device, baudrate=115200, timeout=.2)
+                break
+        if self.reciever is None:
+            raise RuntimeError('Reciever not found in available COM ports')
         self.timing = timing
         self.streaming = False
         self.mode = 1
@@ -15,60 +25,76 @@ class Reciever:
 
     def start_stream(self):
         self.streaming = True
-        t = threading.Timer(self.timing, self.fetch_data)
+        t = threading.Timer(0, self.fetch_data)
         t.start()
 
     def end_stream(self):
         self.streaming = False
 
-    def write_data(self):
-        self.arduino.write('w'.encode('utf8'))
+    def write_data(self, char):
+        self.reciever.write(char.encode('utf8'))
 
     def read_data(self):
         return self.mode, self.x, self.y, self.z
 
     def fetch_data(self):
-        self.arduino.flushInput()
-        time.sleep(0.01)
-        data = self.arduino.read_until(bytes('\n', 'utf-8')).decode("utf-8")
+        self.write_data('w')
+        data = self.reciever.read_until(bytes('\n', 'utf-8')).decode("utf-8")
         parsed = data.split('\t')
         if len(parsed) == 4:
-            parsed = [int(item) for item in parsed]
-            mode = parsed[0]
-            x = parsed[1]
-            y = parsed[2]
-            z = parsed[3]
+            try:
+                parsed = [int(item) for item in parsed]
+                mode = parsed[0]
+                x = parsed[1]
+                y = parsed[2]
+                z = parsed[3]
 
-            self.mode = mode
-            self.x = x
-            self.y = y
-            self.z = z
-
+                self.mode = mode
+                self.x = x
+                self.y = y
+                self.z = z
+            except Exception as e:
+                print(e)
+                pass
         if self.timing > 0 and self.streaming:
-            # t = threading.Timer(self.timing, self.fetch_data)
-            # t.start()
-            time.sleep(self.timing)
-            self.fetch_data()
+            t = threading.Timer(self.timing, self.fetch_data)
+            t.start()
 
         return self.mode, self.x, self.y, self.z
 
     def close(self):
         self.streaming = False
-        self.arduino.close()
+        time.sleep(1)
+        self.reciever.close()
 
 
 def main():
-    reciever = Reciever()
+    reciever = Reciever(timing=0.01)
     reciever.start_stream()
 
-    # reciever.write_data()
-    # reciever.close()
-
+    # for i in range(100):
     while True:
         mode, x, y, z = reciever.read_data()
         print(mode, x, y, z)
-        time.sleep(2)
+        time.sleep(.25)
 
+    reciever.end_stream()
+    print('Ending Stream')
+
+    for i in range(8):
+        mode, x, y, z = reciever.read_data()
+        print(mode, x, y, z)
+        time.sleep(.5)
+
+    reciever.start_stream()
+    print('Starting Stream')
+
+    for i in range(20):
+        mode, x, y, z = reciever.read_data()
+        print(mode, x, y, z)
+        time.sleep(.5)
+
+    reciever.close()
 
 
 if __name__ == '__main__':
--- Testing/Arduino Code/Trackball_Sim/Trackball_Sim.ino
+++ Testing/Arduino Code/Trackball_Sim/Trackball_Sim.ino
@@ -1,40 +1,46 @@
 // Trackball_Sim.ino
 
-#define POT_PIN A0
 #define SERIAL_BAUD 115200
+#define X_POT_PIN A0
+#define Y_POT_PIN A1
+#define Z_POT_PIN A2
 
-char incomingChar = 'w';
+
+char incomingChar = '\0';
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
+  while (!Serial) {
+  }
+//  Serial.write("ready");
 }
 
 void loop() {
-    sendData();
-
+    // recieved new command
     if (Serial.available() > 0) {
       incomingChar = Serial.read();
-      Serial.print("received: ");
-      Serial.println(incomingChar);
-      // send request to mouse
-  }
 
+      if (incomingChar == 'w') {  // recieved 'w' command
+        // send data request to mouse, get new data
+        sendData();     // send new data
+      }
+    }
     delay(100);
 }
 
 void sendData(){
-    int potVal = analogRead(POT_PIN);   // 0 - 1023
-    int x_test = potVal;                // 0 - 1023
-    int y_test = (potVal * 3) % 1023;   // 0 - 1023
-    int z_test = (potVal + 650) % 1023; // 0 - 1023
-    int modeOfOperation = 1;            // 1 - 3
+    int x = analogRead(X_POT_PIN);         // 0 - 1023
+    int y = analogRead(Y_POT_PIN);         // 0 - 1023
+    int z = analogRead(Z_POT_PIN);         // 0 - 1023
+    int mode = 1;                          // 1 - 3
 
-    Serial.print(modeOfOperation, DEC);
+    // send mode\tx\ty\tz\n
+    Serial.print(mode, DEC);
     Serial.print('\t');
-    Serial.print(x_test, DEC);
+    Serial.print(x, DEC);
     Serial.print('\t');
-    Serial.print(y_test, DEC);
+    Serial.print(y, DEC);
     Serial.print('\t');
-    Serial.print(z_test, DEC);
+    Serial.print(z, DEC);
     Serial.print('\n');
 }
