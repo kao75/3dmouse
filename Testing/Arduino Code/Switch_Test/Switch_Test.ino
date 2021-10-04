7e057a02e348e3531cf090e10b72fadd28543705
dbutler6250 <dbutler6250@gmail.com>
2021-10-04 19:09:33 -0400

    Create Switch Test Software

---
+++ Testing/Arduino Code/Switch_Test/Switch_Test.ino
@@ -0,0 +1,15 @@
+/*
+ * Switch_Test.ino
+ * Used for 1st checkoff for ECE 1896: Interface with mode switches and LEDs
+ * Created By: Dylan Butler
+ * Last Modified: 10/4/2021
+ */
+
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
